5e1eaab5bdc35bc29a9c1beaf502515008b8a7fb
McCain <2996993072@qq.com>
2024-07-26 20:48:34 +0800

    have finished the map_init and the map_print

--- src/map.c
+++ src/map.c
@@ -1,17 +1,19 @@
-void map_init(Map *map);
-void map_print(Map *map);
+#include "map.h"
+#include <stdio.h>
 
 void map_init(Map *map)
 {
+    //初始化show_char
     for (int i = 0; i < 70; i++)
     {
         map->cells[i].show_char = '0';
         map->cells[i].kind = 0;
         map->cells[i].rank = 0;
         map->cells[i].has_tool = 0;
+        map->cells[i].owner = 0;
     }
 
-    for (int i=64,i<70;i++ )
+    for (int i=64;i<70;i++ )
     {
         map->cells[i].show_char = '$';
     }
@@ -22,6 +24,36 @@ void map_init(Map *map)
     map->cells[63].show_char = 'M';
     map->cells[14].show_char = 'H';
     map->cells[49].show_char = 'P';
+
+    //初始化kind
+    map->cells[0].kind = 4;
+    map->cells[28].kind = 4;
+    map->cells[35].kind = 4;
+    map->cells[63].kind = 4;
+    map->cells[14].kind = 4;
+    map->cells[49].kind = 4;
+
+    for (int i = 64; i < 70; i++)
+    {
+        map->cells[i].kind = 4;
+    }
+
+    //初始化rank
+    for(int i=0;i<29;i++)
+    {
+        map->cells[i].rank = 1;
+    }
+
+    for(int i=29;i<35;i++)
+    {
+        map->cells[i].rank = 2;
+    }
+
+    for(int i=35;i<64;i++)
+    {
+        map->cells[i].rank = 3;
+    }
+    
 }
 
 void map_print(Map *map)
@@ -29,8 +61,8 @@ void map_print(Map *map)
     for (int i = 0; i < 29; i++)
     {
         printf("%c", map->cells[i].show_char);
-        printf("\n");
     }
+    printf("\n");
 
     //第二行
     printf("%c", map->cells[69].show_char);
@@ -89,6 +121,7 @@ void map_print(Map *map)
     for (int i = 63; i > 34; i--)
     {
         printf("%c", map->cells[i].show_char);
-        printf("\n");
+        
     }
+    printf("\n");
 }
