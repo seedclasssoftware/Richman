bbae9b693d2bd337c7755d6c0320dc5f3efdc590
Gujingyi <2856514560@qq.com>
2024-07-26 20:32:12 +0800

    继续加

--- src/buy_earth.c
+++ src/buy_earth.c
@@ -1,4 +1,6 @@
 #include "players.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 uint32_t buy_earth(Players players)
 {
@@ -6,7 +8,7 @@ uint32_t buy_earth(Players players)
 
 }
 
-void test_buy_earth_when_money_is_200()
+void test1_buy_earth1_when_money_is_200()
 {
     Players players;
     players.money = 200;
@@ -16,6 +18,19 @@ void test_buy_earth_when_money_is_200()
     players.money = buy_earth(players);
     if(players.money == 0)
     {
-        printf("Success.")
+        printf("1Success.");
     }
+    else {
+        printf("1Failed.");
+    }
+}
+
+void test2_buy_earth2_when_money_is_200()
+{
+    Players players;
+    players.money = 200;
+    players.name = "QIAN_Madam";
+    players.number = 1;
+    players.position = 29;
+    
 }
