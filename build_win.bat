e8320f89f3d25ba5b7497639d0ef175051f8a084
wangyihe <heheyoumingwyh@qq.com>
2024-07-26 11:51:54 +0800

    chore: 更新Windows构建脚本，添加依赖项安装步骤和配置说明

--- build_win.bat
+++ build_win.bat
@@ -1,23 +1,19 @@
 @echo off
 
-REM 安装依赖项
 conan install . --build=missing --profile=conan_profile_x64
 
-REM 创建构建目录
 if not exist build (
     mkdir build
 )
 
 cd build
 
-REM 配置 CMake 项目
 cmake .. -G "Ninja" ^
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
     -DCMAKE_BUILD_TYPE=Debug ^
     -DCMAKE_TOOLCHAIN_FILE="%CD%\Release\generators\conan_toolchain.cmake" ^
     -DCMAKE_PREFIX_PATH="%CD%\Release\generators"
 
-REM 构建项目
 cmake --build . --config Debug
 
 cd ../out
