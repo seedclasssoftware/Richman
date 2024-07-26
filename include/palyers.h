c5c2604422e2404f7336981571a1285662e7a7e3
wangyihe <heheyoumingwyh@qq.com>
2024-07-26 17:11:38 +0800

    chore: 添加玩家结构体定义和属性到players.h文件中

--- include/palyers.h
+++ include/palyers.h
@@ -46,8 +46,32 @@ enum Players_Name {
  *
  * @param name 玩家名{char*}
  * @param money 玩家金钱{uint32_t}
- * @param 
- * @param 
+ * @param point 玩家道具点数{uint32_t}
+ * @param number 玩家编号{uint8_t}
+ * @param block 道具:障碍{uint8_t}
+ * @param robot 道具:机器娃娃{uint8_t}
+ * @param bomb 道具:炸弹{uint8_t}
+ * @param god 增益:财神{uint8_t} 使用1-5代表剩余回合数,0表示未使用
+ * @param prison 增益:监狱{uint8_t} 使用1-2代表剩余回合数,0表示未使用
+ * @param hospital 增益:医院{uint8_t} 使用1-3代表剩余回合数,0表示未使用
+ * @param magic 增益:魔法{uint8_t} 使用1-3代表剩余回合数,0表示未使用
+ * @param position 玩家位置{uint8_t}
+ * @param properties 玩家资产 ...未实现
  */
+typedef struct Players {
+  char *name;
+  uint32_t money;
+  uint32_t point;
+  uint8_t number;
+  uint8_t block;
+  uint8_t robot;
+  uint8_t bomb;
+  uint8_t god;
+  uint8_t prison;
+  uint8_t hospital;
+  uint8_t magic;
+  uint8_t position;
+  /// properties
+} Players, *pPlayers;
 
 #endif ///< PLAYERS_H
