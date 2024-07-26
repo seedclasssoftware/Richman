2878269e40bed36932d761cbf8bee88647158dfe
wangyihe <heheyoumingwyh@qq.com>
2024-07-26 16:40:51 +0800

    chore: 添加玩家结构体定义到players.h文件中

---
+++ include/palyers.h
@@ -0,0 +1,53 @@
+/**
+ * @file palyers.h
+ * @author 王一赫 (wyihe5220@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-07-26
+ *
+ * @copyright copyright (c) 2024
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *   or more contributor license agreements.  See the NOTICE file
+ *   distributed with this work for additional information
+ *   regarding copyright ownership.  The ASF licenses this file
+ *   to you under the Apache License, Version 2.0 (the
+ *   "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ *
+ */
+
+#ifndef PLAYERS_H
+#define PLAYERS_H
+
+#include <stdint.h>
+
+/**
+ * @brief 枚举玩家名字宏定义
+ *
+ */
+enum Players_Name {
+  ATUB = 0,   ///< 阿土伯 = 0
+  QIAN_Madam, ///< 钱夫人 = 1
+  SUN_Miss,   ///< 孙小姐 = 2
+  JIN_Bei,    ///< 金贝 = 3
+};
+
+/**
+ * @brief 玩家结构体
+ *
+ * @param name 玩家名{char*}
+ * @param money 玩家金钱{uint32_t}
+ * @param 
+ * @param 
+ */
+
+#endif ///< PLAYERS_H
