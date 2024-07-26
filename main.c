4e8ab8223485c1cd01a05ee73f2e90f2185e263b
wangyihe <heheyoumingwyh@qq.com>
2024-07-26 20:36:15 +0800

    refactor: Update player name initialization and printing functions

--- main.c
+++ main.c
@@ -31,6 +31,8 @@
 
 Players players[4];
 
+pPlayers now_user = &players[0];
+
 int main(int argc, char const *argv[], char const *envp[])
 {
     if (argc == 0)
@@ -55,7 +57,6 @@ int main(int argc, char const *argv[], char const *envp[])
     printPlayers(players, 4);
     }
     SetConsoleOutputCP(CP_UTF8);///< 设置控制台输出编码为UTF-8,很重要,否则中文输出乱码
-    // 打开文件
 
     return 0;
 }
