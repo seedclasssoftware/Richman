bc17079526208268aab7e1664e74b56452cfcde3
moaila <2753661300@qq.com>
2024-07-27 10:16:05 +0800

    01

--- src/roll.c
+++ src/roll.c
@@ -2,5 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-int roll_num()
+int roll_num() {
+    // 使用当前时间作为种子
+    srand(time(NULL));
+
+    // 生成1到6之间的随机数
+    int random_number = rand() % 6 + 1;
+
+    
+
+    return 0;
+}
