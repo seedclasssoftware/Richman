80123edcc146d50e864cade68f921e4aaca58e49
Gujingyi <2856514560@qq.com>
2024-07-27 13:21:12 +0800

    加了名字注释

--- src/tool_house.c
+++ src/tool_house.c
@@ -1,3 +1,29 @@
+/**
+ * @file tool_house.c
+ * @author 古静怡 (2856514560@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-07-27
+ * 
+ * @copyright copyright (c) 2024
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *   or more contributor license agreements.  See the NOTICE file
+ *   distributed with this work for additional information
+ *   regarding copyright ownership.  The ASF licenses this file
+ *   to you under the Apache License, Version 2.0 (the
+ *   "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ * 
+ */
 #include "tool_house.h"
 #include "players.h"
 
