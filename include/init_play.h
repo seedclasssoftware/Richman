d274d5c3d743a716f6bb55c779232875e3c35a81
wangyihe <heheyoumingwyh@qq.com>
2024-07-27 09:42:22 +0800

    refactor: Add functions to initialize game money and select players

--- include/init_play.h
+++ include/init_play.h
@@ -4,7 +4,21 @@
 #include <stdint.h>
 #include "players.h"
 
+/**
+ * @brief Initializes the money for the game
+ * 
+ * @param initMoney Pointer to the variable that will store the initial money
+ */
 void init_money(uint32_t *initMoney);
+
+/**
+ * @brief Selects the players for the game
+ * 
+ * @param players Pointer to the players struct
+ * @param selected_players Pointer to the array that will store the selected players
+ * @param num_players Number of players
+ * @param init_money Initial money for the game
+ */
 void select_players(Players *players, int *selected_players, int num_players, uint32_t init_money);
 
 #endif
