0fecf1f222b4ad0fe173041c1e9489029eeccfcc
moaila <2753661300@qq.com>
2024-07-27 15:45:31 +0800

    fix

--- main.c
+++ main.c
@@ -47,7 +47,6 @@ pPlayers now_user = &players[0];
 
 int main(int argc, char const *argv[], char const *envp[]) {
 
-  /*
   SetConsoleOutputCP(
       CP_UTF8); ///< 设置控制台输出编码为UTF-8,很重要,否则中文输出乱码
   /// 无参数,默认为游戏模式
