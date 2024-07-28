30bfc890cf8747d12ccc385a6a797f68b1a6ce52
wangyihe <heheyoumingwyh@qq.com>
2024-07-28 18:14:46 +0800

    refactor: Update player numbers and fix debuff conditions

--- src/interaction.c
+++ src/interaction.c
@@ -29,15 +29,14 @@
 #include "players.h"
 #include "query.h"
 #include "roll.h"
-#include "useprops.h"
 #include "sell_house.h"
+#include "useprops.h"
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
-
 
 extern pPlayers now_user;
 extern Map map;
