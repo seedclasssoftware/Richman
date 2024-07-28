178a6e67c7ea61dad678735b32bfc842b4f64173
wangyihe <heheyoumingwyh@qq.com>
2024-07-28 13:01:29 +0800

    refactor: Remove trailing whitespace in map.h

--- include/map.h
+++ include/map.h
@@ -31,7 +31,7 @@ typedef struct Cell {
   char show_char;
   // 字符 S 起点、 T 道具屋、 $ 矿地、 H 医院
   // M 魔法屋、G 礼品屋、P 监狱、0 普通地段
-
+  
   int kind; // 地的种类  0 空地 1 茅屋 2 洋房 3 摩天楼 特殊建筑为4
   int rank;     // 地段      1 地段一 2 地段二 3 地段三
   int has_tool; // 道具  1 路障、3 炸弹、0 没有道具
