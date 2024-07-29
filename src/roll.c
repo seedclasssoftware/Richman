35724cb413843fc6b3288001eeb7a600786d1a47
wangyihe <heheyoumingwyh@qq.com>
2024-07-29 14:30:39 +0800

    chore: Remove commented-out code in roll.c

--- src/roll.c
+++ src/roll.c
@@ -38,6 +38,7 @@
 #include <string.h>
 #include <time.h>
 
+
 // #include "winnt.h"
 extern Map map;
 extern Players players[4];
@@ -84,30 +85,34 @@ void change_position(pPlayers now_user, int steps) {
       temp[now_user->position][3] ? temp[now_user->position][3]
                                   : map.cells[now_user->position].init_char;
   for (int i = 1; i <= steps; i++) {
-    /*     int tool = map.cells[(now_user->position + i)%70].has_tool;
-        if (tool == 0)
-          continue;
-        else if (tool == 1) {
-          printf("很不幸！你碰到了路障，止步于此！\n");
-          map.cells[(now_user->position + i)%70].has_tool = 0;
-          map.cells[(now_user->position + i)%70].show_char =
-       map.cells[(now_user->position + i)%70].init_char; now_user->position +=
-       (uint8_t)i; now_user->position %= 70; change_show(now_user);
-          map.cells[now_user->position].show_char =
-              temp[now_user->position][3] ? temp[now_user->position][3]
-                                          :
-       map.cells[now_user->position].init_char; flag = 1; break; } else if (tool
-       == 3) { printf("很不幸！你碰到了炸弹，请在医院休息三天！\n");
-          map.cells[(now_user->position + i)%70].has_tool = 0;
-          map.cells[(now_user->position + i)%70].show_char =
-       map.cells[(now_user->position + i)%70].init_char; now_user->position =
-       (uint8_t)14; now_user->hospital = (uint8_t)3; change_show(now_user);
-          map.cells[now_user->position].show_char =
-              temp[now_user->position][3] ? temp[now_user->position][3]
-                                          :
-       map.cells[now_user->position].init_char; flag = 1; break;
-        } */
-        
+    int tool = map.cells[(now_user->position + i)%70].has_tool;
+    if (tool == 0)
+      continue;
+    else if (tool == 1) {
+      printf("很不幸！你碰到了路障，止步于此！\n");
+      map.cells[(now_user->position + i)%70].has_tool = 0;
+      map.cells[(now_user->position + i)%70].show_char = map.cells[(now_user->position + i)%70].init_char;
+      now_user->position += (uint8_t)i;
+      now_user->position %= 70;
+      change_show(now_user);
+      map.cells[now_user->position].show_char =
+          temp[now_user->position][3] ? temp[now_user->position][3]
+                                      : map.cells[now_user->position].init_char;
+      flag = 1;
+      break;
+    } else if (tool == 3) {
+      printf("很不幸！你碰到了炸弹，请在医院休息三天！\n");
+      map.cells[(now_user->position + i)%70].has_tool = 0;
+      map.cells[(now_user->position + i)%70].show_char = map.cells[(now_user->position + i)%70].init_char;
+      now_user->position = (uint8_t)14;
+      now_user->hospital = (uint8_t)3;
+      change_show(now_user);
+      map.cells[now_user->position].show_char =
+          temp[now_user->position][3] ? temp[now_user->position][3]
+                                      : map.cells[now_user->position].init_char;
+      flag = 1;
+      break;
+    }
   }
   if (flag == 0) {
     now_user->position += steps;
